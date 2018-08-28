35fffbab0f2b5b4c13e35115213656ee952be114
Albin Hübsch <albin.hubsch@gmail.com>
2018-08-28 21:11:20 +0200

    v1 now supports pluralization

--- index.js
+++ index.js
@@ -2,22 +2,66 @@
 const chalk = require('chalk')
 const download = require('./src/download')
 const utils = require('./src/utils')
-
+const fs = require('fs')
+const DIR = process.cwd()
 const [,, ...args] = process.argv
 
-console.log(chalk.yellow(args))
-
-console.log(utils.getConfig())
-
+// console.log(chalk.yellow(args))
 // console.log(chalk.blue.bgRed.bold('Hello world!'))
 
-return
+// Prepare google sheet url
+const googleSheetKey = utils.getConfig().key
+const url = `https://docs.google.com/feeds/download/spreadsheets/Export?key=${googleSheetKey}&exportFormat=csv`
 
-// Retrieve document key
-const key = JSON.parse(packagejson).copyDoc
+function writeFiles(TRANSLATIONS, translationsMap){
 
-// Prepare google play url
-const url = `https://docs.google.com/feeds/download/spreadsheets/Export?key=${key}&exportFormat=csv`
+    let langCount = 0
+
+    for (const lang in translationsMap) {
+        langCount++
+        const fileName = `${utils.getConfig().directory}/${translationsMap[lang]}.js`
+
+        const stream = fs.createWriteStream(fileName, { flags: 'w+' })
+        stream.once('open', function (fd) {
+            stream.write("export default {\n")
+
+            for (const transKey in TRANSLATIONS[translationsMap[lang]]) {
+
+                if (TRANSLATIONS[translationsMap[lang]][transKey] !== null && typeof TRANSLATIONS[translationsMap[lang]][transKey] === 'object' ){
+                    
+                    const pluralList = [' {\n']
+                    for (const typeKey of Object.keys(TRANSLATIONS[translationsMap[lang]][transKey])) {
+                        if (TRANSLATIONS[translationsMap[lang]][transKey][typeKey].length > 0){
+                            pluralList.push(
+                                `        ${typeKey}: '${TRANSLATIONS[translationsMap[lang]][transKey][typeKey].replace(/[\\$'"]/g, "\\$&")}',\n`
+                            )
+                        }
+                    }
+
+                    pluralList.push('    }')
+                    stream.write(`    ${transKey}: ${pluralList.join('')},\n`)
+
+                }else{
+                    if (TRANSLATIONS[translationsMap[lang]][transKey].length > 0){
+                        const value = TRANSLATIONS[translationsMap[lang]][transKey].replace(/[\\$'"]/g, "\\$&")
+                        stream.write(`    ${transKey}: '${value}',\n`)
+                    }
+                }
+            }
+
+            stream.write("}")
+            stream.end()
+        })
+    }
+
+    // Remove temp file
+    fs.unlink("tmp_copyDoc.csv", ()=>{})
+
+    console.log(`${chalk.green.bold('DONE')}: Generated ${chalk.bold(langCount)} language files`)
+    for (const lang in translationsMap) {
+        console.log(`  - ${chalk.blue(translationsMap[lang])}    ${chalk.underline.rgb(160, 160, 160)(`${utils.getConfig().directory}/${translationsMap[lang]}.js`)}`)
+    }
+}
 
 download(url, 'tmp_copyDoc.csv', () => {
 
@@ -28,6 +72,7 @@ download(url, 'tmp_copyDoc.csv', () => {
         if (index === arr.length - 1 && line === "") { return }
 
         const split = line.split(',')
+        const type = split[2]
 
         if (index === 1) {
             for (let i = 3; i < split.length - 2; i++) {
@@ -39,35 +84,28 @@ download(url, 'tmp_copyDoc.csv', () => {
         if (index !== 0 && index !== 1 && split[1]) {
 
             for (const key in translationsMap) {
-                TRANSLATIONS[translationsMap[key]][split[1]] = split[key]
+
+                if(type){
+                    if ( TRANSLATIONS[translationsMap[key]][split[1]] ){
+                        TRANSLATIONS[translationsMap[key]][split[1]][type] = split[key]
+                    }else{
+                        TRANSLATIONS[translationsMap[key]][split[1]] = {
+                            [type]: split[key]
+                        }
+                    }
+                }else{
+                    TRANSLATIONS[translationsMap[key]][split[1]] = split[key]
+                }
             }
         }
     })
 
-    // WRITE TO FILES
-    let langCount = 0
-    for (const lang in translationsMap) {
-        langCount++
-        const fileName = `./resources/locales/${translationsMap[lang]}.js`
-
-        const stream = fs.createWriteStream(fileName)
-        stream.once('open', function (fd) {
-            stream.write("export default {\n")
-
-            for (const transKey in TRANSLATIONS[translationsMap[lang]]) {
-
-                const value = TRANSLATIONS[translationsMap[lang]][transKey].replace(/[\\$'"]/g, "\\$&")
-
-                stream.write(`    ${transKey}: '${value}',\n`)
-            }
-
-            stream.write("}")
-            stream.end()
+    try {
+        utils.mkdir_p(`${utils.getConfig().directory}`, null, ()=>{
+            writeFiles(TRANSLATIONS, translationsMap)
         })
-    }
-
-    console.log(`DONE : Generated ${langCount} language files`)
-    for (const lang in translationsMap) {
-        console.log(`  - ${translationsMap[lang]}    (./resources/locales/${translationsMap[lang]}.js)`)
+    } catch (error) { 
+        console.log(chalk.yellow.bgRed.bold('ERROR: sry something went wrong...'))
+        console.log(error.message)
     }
 })
--- src/utils.js
+++ src/utils.js
@@ -21,7 +21,41 @@ const getConfig = function (){
     return
 }
 
+function mkdir_p(path, mode, callback, position) {
+    mode = mode || 0777;
+    position = position || 0;
+    parts = require('path').normalize(path).split('/');
+
+    if (position >= parts.length) {
+        if (callback) {
+            return callback();
+        } else {
+            return true;
+        }
+    }
+
+    var directory = parts.slice(0, position + 1).join('/');
+    fs.stat(directory, function (err) {
+        if (err === null) {
+            mkdir_p(path, mode, callback, position + 1);
+        } else {
+            fs.mkdir(directory, mode, function (err) {
+                if (err) {
+                    if (callback) {
+                        return callback(err);
+                    } else {
+                        throw err;
+                    }
+                } else {
+                    mkdir_p(path, mode, callback, position + 1);
+                }
+            })
+        }
+    })
+}
+
 module.exports = {
+    mkdir_p: mkdir_p,
     getCSVUri: getCSVUri,
     getConfig: getConfig
 }
